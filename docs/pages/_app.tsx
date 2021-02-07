10b6c4dacc6258b08e0869d16f846803ebe72bbe
Carlos Baraza Haro <carlos.baraza@ieee.org>
2021-02-07 13:17:43 +0000

    Remove docs

--- docs/pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import { AppProps } from "next/app";
-import "dashvar/dist/base.css";
-import "dashvar/dist/dashvar.css";
-import "../styles/index.css";
-
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
