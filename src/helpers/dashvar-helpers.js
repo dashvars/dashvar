1cff74122cda4f6d0b2db7cd9dfc4cd0220721d2
Carlos Baraza Haro <carlos.baraza@ieee.org>
2021-02-15 14:23:22 +0000

    add xs media breakpoint

--- src/helpers/dashvar-helpers.js
+++ src/helpers/dashvar-helpers.js
@@ -3,6 +3,8 @@
     var w = window.innerWidth;
     var add = [];
     var del = [];
+    if (w > 480) add.push("xs");
+    else del.push("xs");
     if (w > 640) add.push("sm");
     else del.push("sm");
     if (w > 768) add.push("md");
