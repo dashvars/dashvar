91e717190528783ec1ed9b26620b012c0e34b429
Carlos Baraza Haro <carlos.baraza@ieee.org>
2021-03-09 18:13:55 +0000

    Change the helper to use matchMedia

--- src/helpers/dashvar-helpers.js
+++ src/helpers/dashvar-helpers.js
@@ -1,29 +1,31 @@
 (function () {
-  function dashvarMedia() {
-    var w = window.innerWidth;
+  if (typeof window === "undefined") return;
+  var mm = window.matchMedia || window.msMatchMedia;
+  var minw = function (w) {
+    return mm ? mm("(min-width: " + w + "px)").matches : false;
+  };
+  var dashvarMedia = function () {
     var add = [];
     var del = [];
-    if (w > 480) add.push("xs");
+    if (minw(480)) add.push("xs");
     else del.push("xs");
-    if (w > 640) add.push("sm");
+    if (minw(640)) add.push("sm");
     else del.push("sm");
-    if (w > 768) add.push("md");
+    if (minw(768)) add.push("md");
     else del.push("md");
-    if (w > 1024) add.push("lg");
+    if (minw(1024)) add.push("lg");
     else del.push("lg");
-    if (w > 1280) add.push("xl");
+    if (minw(1280)) add.push("xl");
     else del.push("xl");
-    if (w > 1536) add.push("xxl");
+    if (minw(1536)) add.push("xxl");
     else del.push("xxl");
-
     del.forEach(function (c) {
       document.body.classList.remove(c);
     });
     add.forEach(function (c) {
       document.body.classList.add(c);
     });
-  }
-  if (typeof window === "undefined") return;
+  };
   window.addEventListener("resize", dashvarMedia);
   window.addEventListener("DOMContentLoaded", dashvarMedia);
   window.addEventListener("load", dashvarMedia);
